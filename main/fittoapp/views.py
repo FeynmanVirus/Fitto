a0b0725f514a5308f6e512b2f89b7196b466ce15
FeynmanVirus <134346276+FeynmanVirus@users.noreply.github.com>
2023-08-02 17:08:56 +0530

    migrated authentication to anothe app called users

--- main/fittoapp/views.py
+++ main/fittoapp/views.py
@@ -2,20 +2,19 @@ from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
 import json 
-from .models import User, FoodDiary # db
-from .forms import NewUserForm # form
+from .models import FoodDiary # db
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm #login
-from django.contrib import messages # form
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+import datetime
 
-
-# Create your views here.
+# Create your views here. 
 def index(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('fittoapp:register'))
+        return HttpResponseRedirect(reverse('login'))
     return render(request, "fittoapp/index.html")
 
+@login_required
 def calculate_calories(request):
     return render(request, "fittoapp/calc.html")
 
@@ -25,46 +24,11 @@ def send(request):
         xml_fetched_bytesvalue = request.body
         datajson = xml_fetched_bytesvalue.decode("utf-8").replace("'", '"')
         data = json.loads(datajson)
-        
-        insert = FoodDiary()
+        print(data['PROCNT'])
 
+        b = FoodDiary(user=request.user, energy=data['ENERC_KCAL'], protein=data['PROCNT'], carbs=data['CHOCDF'], fat=data['FAT'],  fiber=data['FIBTG'], date=datetime.date.today())
+        b.save()
+        print(b)
         return HttpResponse('success')
     return HttpResponse('failed')
 
-def register_request(request):
-    if request.method=='POST':
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registration successful!")
-            return redirect("fittoapp:index")
-        messages.error(request, "Registration unsuccessful. Please try again with valid info.")
-    form = NewUserForm()
-    return render(request, "fittoapp/register.html", {
-        "register_form":form
-        })
-
-def login_request(request):
-    if request.method=="POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect("fittoapp:index")
-            else:
-                messages.error(request, "Invalid username or password")
-        else:
-            messages.error(request, "Invalid username or password")
-    form = AuthenticationForm()
-    return render(request, 'fittoapp/login.html', {
-        "login_form": form,
-    })
-
-def logout_request(request):
-    logout(request)
-    messages.info(request, "You have successfully logged out.") 
-    return redirect("fittoapp:index")
